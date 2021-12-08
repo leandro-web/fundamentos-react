0707b43559e414990e50ee537509ad703c01efd1
leandro-web <leandro-web@hotmail.com>
2021-12-07 21:41:13 -0300

    enviando parametro

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -4,10 +4,15 @@ import React from 'react'
 import './index.css'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro
+            titulo="Segundo Componente"
+            subtitulo="Muito legal!"
+        />        
     </div>,
     document.getElementById('root')
 )
